30ea2257b585f2cf148dda128baf9c5d51844d14
AirFast <airfast.88@gmail.com>
2021-07-08 17:40:02 +0300

    init

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
