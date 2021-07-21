2e44826643d0b24f79c0ba96a1efa4864810c69f
AirFast <airfast.88@gmail.com>
2021-07-21 15:31:16 +0300

    app

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,9 +3,24 @@ const router = express.Router();
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'GET /products'
-    });
+    Product.find().exec()
+        .then(docs => {
+            console.log(docs);
+
+            if (docs.length) {
+                res.status(200).json(docs);
+            } else {
+                res.status(404).json({
+                    message: 'No entries in the database'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
@@ -13,25 +28,46 @@ router.post('/', (req, res, next) => {
         name: req.body.name,
         price: req.body.price
     });
-    
-    product.save().then(result => {
-        console.log(result);
-    }).catch(err => {
-        console.log(err);
-    });
 
-    res.status(201).json({
-        message: 'POST /products',
-        product
-    });
+    product.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'POST /products',
+                product: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+
 });
 
 router.get('/:id', (req, res, next) => {
     const { id } = req.params;
 
-    res.status(200).json({
-        message: `GET /products/${id}`
-    });
+    Product.findById(id).exec()
+        .then(doc => {
+            console.log(doc);
+
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({
+                    message: 'No entries in the database for this ID'
+                });
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.patch('/:id', (req, res, next) => {
@@ -45,9 +81,16 @@ router.patch('/:id', (req, res, next) => {
 router.delete('/:id', (req, res, next) => {
     const { id } = req.params;
 
-    res.status(200).json({
-        message: `DELETE /products/${id}`
-    });
+    Product.remove({ _id: id }).exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 module.exports = router;
